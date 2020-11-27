5a47a74fd817b005a7da7fa367d934f8927eb67b
soobin00 <66305249+soobin00@users.noreply.github.com>
2020-11-27 19:45:41 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '65-5h65lk75dit@9zxsqit-+nfcotwz7_fe!ebj36+q%a80f1!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
